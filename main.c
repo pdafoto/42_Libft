399e10e10779750873b3ee043d885ab27b36d902
Nicolas Perez De Agustin <nperez-d@c3r11s5.42madrid.com>
2023-09-13 15:47:20 +0200

    modified isalpa, added main and summary txt

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nperez-d <nperez-d@studen.42madrid.com>    +#+  +:+       +#+        */
+/*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 11:22:46 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/09/13 15:44:24 by nperez-d         ###   ########.fr       */
+/*   Created: 2023/09/13 15:40:39 by nperez-d          #+#    #+#             */
+/*   Updated: 2023/09/13 15:43:20 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_isalpha(int c)
+#include <stdio.h>
+int	ft_isalpha(int c);
+
+int	main(int argc, char *argv[])
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1);
-	else
-		return (0);
+	int	c;
+	int	res;
+
+	(void)argc;
+	c = *argv[1];
+	res = ft_isalpha(c);
+	printf("%d:\n%d\n", c, res);
+	return (0);
 }
