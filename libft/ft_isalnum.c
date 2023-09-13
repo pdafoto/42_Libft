b5d3d8d64ba6fadd3f6a0f277ec7f7747cbd5d90
Nicolas Perez De Agustin <nperez-d@c3r11s5.42madrid.com>
2023-09-13 16:39:30 +0200

    added isalnum

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 16:26:34 by nperez-d          #+#    #+#             */
+/*   Updated: 2023/09/13 16:32:54 by nperez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_isalnum(int c)
+{
+	if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	int	c;
+	int	res;
+
+	(void)argc;
+	c = *argv[1];
+	res = ft_isalnum(c);
+	printf("%d:\n%d\n", c, res);
+	return (0);
+}*/
