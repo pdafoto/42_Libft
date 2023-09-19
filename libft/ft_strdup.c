f429bb14e10f0b34c914abe3cef63fdc2aba9d1f
Nicolas Perez De Agustin <nperez-d@c3r3s6.42madrid.com>
2023-09-19 20:39:23 +0200

    added strdup, updated summary

---
+++ libft/ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 20:26:33 by nperez-d          #+#    #+#             */
+/*   Updated: 2023/09/19 20:38:33 by nperez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	int		len;
+	int		i;
+	char	*s2;
+
+	len = 0;
+	while (s1[len])
+		len++;
+	s2 = (char *)malloc(len * sizeof(char));
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	return (s2);
+}
+/*
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char	*s2;
+
+	(void)argc;
+	s2 = ft_strdup(argv[1]);
+	printf("%s", s2);
+	return (0);
+}*/
