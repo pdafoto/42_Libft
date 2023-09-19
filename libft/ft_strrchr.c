92da5d49eea99e92c3854b00e68e0c98ffd17f76
Nicolas Perez De Agustin <nperez-d@c3r3s6.42madrid.com>
2023-09-19 16:38:32 +0200

    added strrchr, updated summary

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 16:07:22 by nperez-d          #+#    #+#             */
+/*   Updated: 2023/09/19 16:35:49 by nperez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*final;
+
+	final = NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			final = (char *)s;
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (final);
+}
+/*
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	char	c;
+
+	(void)argc;
+	printf("%s\n", argv[1]);
+	printf("Introduce el caracter a buscar: ");
+	scanf(" %c", &c);
+	printf("1ª coincidencia: %s\n", ft_strrchr(argv[1], c));
+	return (0);
+}*/
