5b723b948f22985d15704ae6cd7d2fc2d3bf4711
Nicolás <sourcetree@pdafoto.es>
2023-09-22 09:48:42 +0200

    updated strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:14:32 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/09/21 18:47:35 by nperez-d         ###   ########.fr       */
+/*   Updated: 2023/09/22 09:47:34 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s2;
-	int		s1_len;
-	int		i;
-	int		j;
-	int		k;
+	size_t	i;
 
-	s1_len = ft_strlen(s1);
-	s2 = (char *)malloc(s1_len * sizeof(char));
-	if (s2 == NULL)
+	if (s1 == NULL || set == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
-	k = 0;
-	while (i < s1_len)
-	{
-		while (set[j])
-		{
-			if (s1[i] == set[j])
-				s2[k] = 
-		}
-	}
+	while (s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, *(s1 + i)))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
+}
+
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("String: %s\n", argv[1]);
+	printf("Trim: %s\n", argv[2]);
+	printf("Result: %s\n", ft_strtrim(argv[1], argv[2]));
+	return (0);
 }
