7de47afbe6c07d6a6da0e375e77f465d92bb9ec7
Nicolas Perez De Agustin <nperez-d@c3r5s4.42madrid.com>
2023-09-28 16:44:25 +0200

    updated strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 15:02:38 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/09/20 17:55:13 by nperez-d         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:43:14 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,31 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
-	int				res;
+	unsigned int	res;
 
 	i = 0;
 	res = 0;
-	while (s1[i] && s2[i] && i < n && res == 0)
+	if (n == 0)
+		return (0);
+	while (i < n && (s1[i] || s2[i]))
 	{
-		res = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if ((s1[i] == '\0' || s2[i] == '\0') && i < n && res == 0)
-		res = s1[i] - s2[i];
-	return (res);
+	return (0);
 }
+/*
+#include <stdio.h>
 
-// #include <stdio.h>
+int	main(int argc, char *argv[])
+{
+	unsigned int	size;
 
-// int	main(int argc, char *argv[])
-// {
-// 	unsigned int	size;
-
-// 	(void)argc;
-// 	printf("1: %s\n2: %s\n", argv[1], argv[2]);
-// 	printf("Introduce el número de caracteres a comparar: ");
-// 	scanf("%u", &size);
-// 	printf("1ª diferencia: %d\n", ft_strncmp(argv[1], argv[2], size));
-// 	return (0);
-// }
+	(void)argc;
+	printf("1: %s\n2: %s\n", argv[1], argv[2]);
+	printf("Introduce el número de caracteres a comparar: ");
+	scanf("%u", &size);
+	printf("1ª diferencia: %d\n", ft_strncmp(argv[1], argv[2], size));
+	return (0);
+}*/
