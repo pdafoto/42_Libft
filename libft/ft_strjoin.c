8ee274569399be157bc9b5dde4a30de274bc518d
Nicolás <sourcetree@pdafoto.es>
2023-09-29 20:56:32 +0200

    updated strjoin and strtrim

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 11:15:49 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/09/21 11:44:02 by nperez-d         ###   ########.fr       */
+/*   Updated: 2023/09/29 20:23:06 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	s3 = (char *)malloc((s1_len + s2_len) * sizeof(char));
+	s3 = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (i < s1_len)
+	while (s1[i])
 	{
 		s3[i] = s1[i];
 		i++;
 	}
-	while (j < s2_len)
+	while (s2[j])
 	{
 		s3[i + j] = s2[j];
 		j++;
 	}
+	s3[i + j] = '\0';
 	return (s3);
 }
 /*
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,35 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:14:32 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/09/22 09:49:03 by nperez-d         ###   ########.fr       */
+/*   Updated: 2023/09/29 20:55:16 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		begin;
+	int		end;
+	char	*sub_str;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	begin = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[begin]) && begin <= end)
+		begin++;
+	if (begin > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	sub_str = malloc((end - begin + 2) * sizeof(char));
+	if (sub_str == NULL)
+		return (NULL);
+	ft_strlcpy(sub_str, &s1[begin], end - begin + 2);
+	return (sub_str);
+}
+/* char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 
@@ -24,7 +46,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (i && ft_strchr(set, *(s1 + i)))
 		i--;
 	return (ft_substr(s1, 0, i + 1));
-}
+} */
 /*
 #include <stdio.h>
 
