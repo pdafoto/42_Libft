0d46d31b86427d084beb8c14d592cb5d4a86624d
nperez-d <nperez-d@student.42madrid.com>
2023-10-24 12:44:30 +0200

    polished strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -34,19 +34,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(sub_str, &s1[begin], end - begin + 2);
 	return (sub_str);
 }
-/* char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	i;
-
-	if (s1 == NULL || set == NULL)
-		return (NULL);
-	while (s1 && ft_strchr(set, *s1))
-		s1++;
-	i = ft_strlen(s1);
-	while (i && ft_strchr(set, *(s1 + i)))
-		i--;
-	return (ft_substr(s1, 0, i + 1));
-} */
 /*
 #include <stdio.h>
 
